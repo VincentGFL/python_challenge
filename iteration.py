aae7e528d1e888ae04dce0a85a6c72ce0d1b5033
vincent-lin <linguofu303@gmail.com>
2020-10-23 19:25:39 +0100

    Complete iteration tutorial

---
+++ iteration.py
@@ -0,0 +1,24 @@
+count = 0 
+name = str(input("What is your name?"))
+
+while count < 5:
+    print(name, "is awesome!")
+    count += 1
+
+#------------------------
+
+for i in range (5,11):
+    print(i)
+
+#------------------------
+
+i = 0
+while i < 5:
+    name = str(input("What is your name?"))
+    print(name, "is awesome!")
+    i += 1
+
+#------------------------
+
+for i in range(10,21,2):
+    print(i)
