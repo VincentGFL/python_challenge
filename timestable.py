f707d10d406144fb7e7f6ac5f26fe148c99abcdf
VincentGFL <VincentGFL@users.noreply.github.com>
2020-10-24 18:47:02 +0100

    Completed timestable.py

---
+++ timestable.py
@@ -0,0 +1,10 @@
+number1 = 1
+while number1 <= 10:    
+    number2 = 0
+    while number2 <= 10:
+        
+        number2 += 1
+        print(number1 * number2, end='\t')
+    print('')
+    number1 += 1 
+    
