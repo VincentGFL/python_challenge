bf6b1d61984c35e34753a31ca7854ec61e7ac46c
vincent-lin <linguofu303@gmail.com>
2020-10-27 10:20:15 +0000

    Changes to conjugation

--- conjugation.py
+++ conjugation.py
@@ -3,8 +3,11 @@
 def wordconjugation(word):
     with open("wordbank.txt", "r") as readfile:
         for line in readfile:
+            #print(line)
             if word in line:
-                return word
+                #return list(word)
+                print(line)
+                return word.partition(word)
                 #return True
                 
     
